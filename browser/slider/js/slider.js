2ca7398220f14503673951b05ab0e1db5aca1b3d
Maxim Usanov <max.usanov@gmail.com>
2018-07-26 10:51:13 +0300

    add slider.js

---
+++ browser/slider/js/slider.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let arrImg = [
+'airmax-jump.png',
+'airmax-on-foot.png',
+'airmax-playground.png',
+'airmax-top-view.png',
+'airmax.png'];
+
+comst slider =document.getElementById('slider');
+let step = 0
+
