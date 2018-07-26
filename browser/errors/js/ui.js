d0b3ee5e2e84b2c063e62c86c4b6686fad59f362
Maxim Usanov <max.usanov@gmail.com>
2018-07-26 11:20:55 +0300

    changed ui.js

--- browser/errors/js/ui.js
+++ browser/errors/js/ui.js
@@ -1,6 +1,8 @@
 'use strict';
+
 function handleClick {
   openMail();
 }
-const button = window.getElementById('open');
+const button = document.getElementById('open');
+
 button.onclick = handleClick();
