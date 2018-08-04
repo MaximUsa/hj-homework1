86ea81415231108225c44656442a0905bdf60c07
Maxim Usanov <max.usanov@gmail.com>
2018-08-04 09:49:06 +0300

    done

--- event-object/skateboard-gallery/js/slider.js
+++ event-object/skateboard-gallery/js/slider.js
@@ -1,5 +1,22 @@
 'use strict';
 
-const galleryNav = document.getElementById('nav')
-const links = galleryNav.getElementsByTagName('a');
-const viewGallery = document.getElementById('view');
+const galleryNavigation = document.getElementById('nav')
+const links = galleryNavigation.getElementsByTagName('a');
+const viewSkate = document.getElementById('view');
+
+
+function show(event){
+	event.preventDefault();
+	viewSkate.src = this.href;
+	if (this.classList.contains('gallery-current')){
+		return;
+	}
+	for (const link of links){
+		link.classList.remove('gallery-current')
+		link.removeAttribute('class')
+	}
+	this.classList.add('gallery-current');
+}
+
+
+Array.from(links).forEach(link => link.addEventListener('click', show));
