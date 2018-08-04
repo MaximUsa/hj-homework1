478d13150ae9eb590e4df50a72aafae77562daec
Maxim Usanov <max.usanov@gmail.com>
2018-08-04 10:48:27 +0300

    done

---
+++ event-object/easter-egg/js/egg.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const nav = document.getElementsByTagName('nav')[0];
+const secret = document.getElementsByClassName('secret')[0];
+let arrKey = [];
+let secretKey = 'KeyY, KeyT, KeyN, KeyJ, KeyK, KeyJ, KeyU, KeyB, KeyZ';
+
+
+function eggs() {
+	arrKey.push(event.code);
+	if (arrKey.join().search(secretKey) != -1){
+		secret.classList.add('visible');
+		return arrKey = [];
+	}else{
+		secret.classList.remove('visible');
+	}
+
+}
+
+function navVisi() {
+	if (event.ctrlKey && event.altKey && event.code === 'KeyT'){
+		nav.classList.add('visible');
+	}else{
+		nav.classList.remove('visible');
+	}
+}
+
+function allEvent(event) {
+	eggs();
+	navVisi();
+}
+
+document.addEventListener('keydown', allEvent);
