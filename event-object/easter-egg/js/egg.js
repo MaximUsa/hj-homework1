a7739876889521ae29d1d03d7f25f8025eb9a8a7
Maxim Usanov <max.usanov@gmail.com>
2018-08-07 13:12:58 +0300

    done

--- event-object/easter-egg/js/egg.js
+++ event-object/easter-egg/js/egg.js
@@ -3,31 +3,31 @@
 const nav = document.getElementsByTagName('nav')[0];
 const secret = document.getElementsByClassName('secret')[0];
 let arrKey = [];
-let secretKey = 'KeyY, KeyT, KeyN, KeyJ, KeyK, KeyJ, KeyU, KeyB, KeyZ';
+let secretKey = "KeyY,KeyT,KeyN,KeyJ,KeyK,KeyJ,KeyU,KeyB,KeyZ";
 
 
 function eggs() {
-	arrKey.push(event.code);
-	if (arrKey.join().search(secretKey) != -1){
-		secret.classList.add('visible');
-		return arrKey = [];
-	}else{
-		secret.classList.remove('visible');
-	}
+    arrKey.push(event.code);
+    if (arrKey.join().search(secretKey) != -1) {
+        secret.classList.add('visible');
+        return arrKey = [];
+    } else {
+        secret.classList.remove('visible');
+    }
 
 }
 
 function navVisi() {
-	if (event.ctrlKey && event.altKey && event.code === 'KeyT'){
-		nav.classList.add('visible');
-	}else{
-		nav.classList.remove('visible');
-	}
+    if (event.ctrlKey && event.altKey && event.code === 'KeyT') {
+        nav.classList.toggle('visible');
+     } // else {
+    //     nav.classList.remove('visible');
+    // }
 }
 
 function allEvent(event) {
-	eggs();
-	navVisi();
+    eggs();
+    navVisi();
 }
 
 document.addEventListener('keydown', allEvent);
