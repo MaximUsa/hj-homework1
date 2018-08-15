590d8a42158ed18e4ac47083f03a7476f3b9dc24
Maxim Usanov <max.usanov@gmail.com>
2018-08-15 19:01:47 +0300

    donecd

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,9 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    if (event.target.tagName === 'TH') {
+        (event.target.dataset.dir == '1') ? event.target.dataset.dir = -1: event.target.dataset.dir = 1;
+        sortTable(event.target.dataset.propName, event.target.dataset.dir);
+        event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+    }
 }
