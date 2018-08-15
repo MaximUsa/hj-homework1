d98578f65c3d2733f9a51ed125dce5332f21d4a9
Maxim Usanov <max.usanov@gmail.com>
2018-08-15 18:44:21 +0300

    done

--- event-bubbling-capturing/menu/js/menu.js
+++ event-bubbling-capturing/menu/js/menu.js
@@ -11,7 +11,9 @@ function toggleMenu(event) {
 }
 
 function openLink(event) {
-  console.log(this.textContent);
+  //console.log(this.textContent);
+  event.preventDefault();
+  event.stopPropagation();
 }
 
 function init(node) {
