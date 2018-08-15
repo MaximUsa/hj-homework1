54648160885c59406805f06476da19d6a3bbb78f
Maxim Usanov <max.usanov@gmail.com>
2018-08-15 18:52:25 +0300

    done

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,11 @@
 'use strict';
+
+
+list.addEventListener('click', cartChek);
+
+function cartChek() {
+    if ((event.target.tagName === 'A') && (event.target.classList.contains('add-to-cart'))) {
+        event.preventDefault();
+        addToCart(event.target.dataset);
+    };
+}
