7c2afe3b5f508a7e716cb5800cd1653e1c625130
Maxim Usanov <max.usanov@gmail.com>
2018-08-22 17:29:38 +0300

    done

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const body = document.querySelector('body')
+
+const connect = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connect.addEventListener('open', () => {
+    showBubbles(connect);
+});
+
+body.addEventListener('click', event => {
+    connect.send(JSON.stringify({
+        x: event.x,
+        y: event.y
+    }));
+})
