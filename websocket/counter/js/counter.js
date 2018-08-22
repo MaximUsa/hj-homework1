b698a6d37de11a130be58849502265c7a1eb8b80
Maxim Usanov <max.usanov@gmail.com>
2018-08-22 17:39:46 +0300

    done

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const outCount = document.querySelector('.counter');
+const outErrors = document.querySelector('.errors');
+
+const connect = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connect.addEventListener('message', event => {
+    let message = JSON.parse(event.data);
+    outCount.innerText = `${message.connections}`;
+    outErrors.value = `${message.errors}`;
+});
+
+window.addEventListener('beforeunload', () => {
+    connection.close(1000)
+});
