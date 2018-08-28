f5a11246a5fb5b8ff72fdb40f68203de00a323d3
Maxim Usanov <max.usanov@gmail.com>
2018-08-28 14:20:37 +0300

    done

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function createElement(node) {
+    if (typeof node === 'string') {
+        return document.createTextNode(node);
+    }
+
+    const el = document.createElement(node.name);
+
+    if (node.props !== null) {
+        Object.keys(node.props).forEach(i => el.setAttribute(i, node.props[i]));
+    }
+    node.childs.forEach(function(item) {
+        if (typeof item === 'string') {
+            el.textContent += item;
+        } else {
+            el.appendChild(createElement(item))
+        }
+    })
+    return el;
+}
